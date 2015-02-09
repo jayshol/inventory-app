2572053451baca8509a09b656123d46cbd1bf6a6
Jayshol <jayshol@gmail.com>
2015-02-09 12:10:29 -0600

    Add inventory.js

---
+++ inventory.js
@@ -0,0 +1,19 @@
+var items = [
+				{
+					name :'Couch',
+					description: 'Ikea this is comfortable!. Although oddly small and modular it may be the most wonderful couch of my life. Oh and so modern'				
+				},
+				{
+					name : 'Laptop',
+					description: 'Beep boop goes the laptop and I create a node awesomeness all day long'
+				},
+				{
+					name: 'foobar',
+					description :'This is a test for the inventory description system. It is only a test.'
+				}
+
+			];
+
+exports.list = function(req, res){
+	res.render('index', {items : items});
+}
